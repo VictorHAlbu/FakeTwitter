ea62619ea9424387423431ca68a2c4556a4da0a4
VictorHAlbu <vh12albuquerque@gmail.com>
2022-04-11 15:44:42 -0300

    relacionamento tables

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+    belongs_to :user  
+    validates :body, presence: true
+    has_many :likes, dependent: :destroy
+    has_many :comments, dependent: :destroy
+
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,2 +1,5 @@
 class Subscription < ApplicationRecord
+  belongs_to :followed, class_name: 'User' #seguido
+  belongs_to :followed_by, class_name: 'User'#quem segue
+  #duas foreign_key 
 end
