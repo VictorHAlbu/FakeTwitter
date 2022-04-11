97ffc9b42d8791ccfe01d61ea9fae91ca5f5661c
VictorHAlbu <vh12albuquerque@gmail.com>
2022-04-11 18:55:18 -0300

    ajustes

--- app/controllers/user/posts_controller.rb
+++ app/controllers/user/posts_controller.rb
@@ -21,18 +21,6 @@ class User::PostsController < UserController
     redirect_to user_profile_path
   end
 
-  def like_toggle
-    post = Post.find(params[:id])
-    like = Like.find_by(user: current_user, post: post)
-
-    if like.present?
-      like.destroy
-      respond_to {|format| format.json { render json: {liked: false, likesCount: post.likes.count}}}
-    else
-      Like.create(user: current_user, post: post)
-      respond_to {|format| format.json { render json: {liked: true, likesCount: post.likes.count}}}
-    end
-  end
 
   private
 
