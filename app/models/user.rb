6adf8e427e212c3a856e8b2eb9c06605d170d4f9
VictorHAlbu <vh12albuquerque@gmail.com>
2022-04-11 10:54:58 -0300

    user new

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :phone, :name, :avatar, presence: true
+  has_one_attached :avatar
+  has_many :posts
+  has_many :comments
+
+  has_many :followers_subscriptions, foreign_key: :followed_id, class_name: 'Subscription'
+  has_many :followers, through: :followers_subscriptions, source: :followed_by
+
+  has_many :following_subscriptions, foreign_key: :followed_by_id, class_name: 'Subscription'
+  has_many :following, through: :following_subscriptions, source: :followed
+
+  scope :potential_to_follow, -> (user) { where.not(id: user.following.pluck(:id)).where.not(id: user.id) }
+  #QUERY TRÁS POTENCIAS SEGUIDORES QUE O USER PODE SEGUIR - QUERY WHERE NOT NÃO TRÁS USER JÁ SEGUIDOS - NEM O PRÓPIO USUÁRIO
+  #pontential_to_follow poderá ser referenciado em qualquer lugar da aplicação como chamada de método da Classe
 end
