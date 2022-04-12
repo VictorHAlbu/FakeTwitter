723b5c1b579a26210a3b8563b923c56d83661056
VICTOR HUGO DE ALBUQUERQUE <67742624+VictorHAlbu@users.noreply.github.com>
2022-04-12 17:12:04 -0300

    Update profile_controller.rb
    search user

--- app/controllers/user/profile_controller.rb
+++ app/controllers/user/profile_controller.rb
@@ -6,8 +6,12 @@ class User::ProfileController < UserController
   end
   
   def potential_to_follow
+    if params[:search].present?
+        @potential_to_follow = User.where("name ILIKE ?", "%#{params[:search]}%").order('id DESC')
+    else    
       @potential_to_follow = User.potential_to_follow(current_user)
       #o escope potential_to_follow criado no model User onde trás ponteciais usuarios a seguir
+    end
   end
 
   def following
